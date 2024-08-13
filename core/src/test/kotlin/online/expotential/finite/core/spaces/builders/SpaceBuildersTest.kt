d12c5f763ef2e0830fedb9d747f58f4a1c77af4f
Paul Cooper <pdcooper80@gmail.com>
2024-08-13 22:58:31 +0100

    #3: Define spaces

---
+++ core/src/test/kotlin/online/expotential/finite/core/spaces/builders/SpaceBuildersTest.kt
@@ -0,0 +1,50 @@
+package online.expotential.finite.core.spaces.builders
+
+import online.expotential.finite.core.testing.TestingEnum
+import online.expotential.finite.core.testing.TestingEnum.VALUE_ONE
+import online.expotential.finite.core.testing.TestingEnum.VALUE_TWO
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SpaceBuildersTest {
+
+    @Test
+    fun testSpaceForClass() {
+        val space = SpaceBuilders.spaceForClass(TestingEnum::class.java)
+            .build()
+        assertEquals("TestingEnum", space.description())
+        assertEquals("Value [VALUE_ONE]", space.describeItem(VALUE_ONE))
+        assertEquals("Value [VALUE_TWO]", space.describeItem(VALUE_TWO))
+    }
+
+    @Test
+    fun testSpaceForClassWithOverriddenDescription() {
+        val space = SpaceBuilders.spaceForClass(TestingEnum::class.java)
+            .withDescription("A testing enumeration")
+            .build()
+        assertEquals("A testing enumeration", space.description())
+        assertEquals("Value [VALUE_ONE]", space.describeItem(VALUE_ONE))
+        assertEquals("Value [VALUE_TWO]", space.describeItem(VALUE_TWO))
+    }
+
+    @Test
+    fun testSpaceForClassWithOverriddenItemDescriber() {
+        val space = SpaceBuilders.spaceForClass(TestingEnum::class.java)
+            .withItemDescriber { "It is ${it.name}" }
+            .build()
+        assertEquals("TestingEnum", space.description())
+        assertEquals("It is VALUE_ONE", space.describeItem(VALUE_ONE))
+        assertEquals("It is VALUE_TWO", space.describeItem(VALUE_TWO))
+    }
+
+    @Test
+    fun testSpaceForClassWithOverriddenDescriptionAndItemDescriber() {
+        val space = SpaceBuilders.spaceForClass(TestingEnum::class.java)
+            .withDescription("A testing enumeration")
+            .withItemDescriber { "It is ${it.name}" }
+            .build()
+        assertEquals("A testing enumeration", space.description())
+        assertEquals("It is VALUE_ONE", space.describeItem(VALUE_ONE))
+        assertEquals("It is VALUE_TWO", space.describeItem(VALUE_TWO))
+    }
+}
--- core/src/test/kotlin/online/expotential/finite/core/spaces/defaults/SpaceDefaultsTest.kt
+++ core/src/test/kotlin/online/expotential/finite/core/spaces/defaults/SpaceDefaultsTest.kt
@@ -1,7 +1,8 @@
 package online.expotential.finite.core.spaces.defaults
 
-import online.expotential.finite.core.spaces.defaults.SpaceDefaultsTest.TestingSpace.VALUE_ONE
-import online.expotential.finite.core.spaces.defaults.SpaceDefaultsTest.TestingSpace.VALUE_TWO
+import online.expotential.finite.core.testing.TestingEnum
+import online.expotential.finite.core.testing.TestingEnum.VALUE_ONE
+import online.expotential.finite.core.testing.TestingEnum.VALUE_TWO
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -9,24 +10,17 @@ class SpaceDefaultsTest {
 
     @Test
     fun testDefaultSpaceDescription() {
-        assertEquals("TestingSpace", SpaceDefaults.defaultSpaceDescription(TestingSpace::class.java))
+        assertEquals("TestingEnum", SpaceDefaults.defaultSpaceDescription(TestingEnum::class.java))
     }
 
     @Test
     fun testDefaultItemDescriber() {
-        assertEquals("Value [VALUE_ONE]", SpaceDefaults.defaultItemDescriber<TestingSpace>().invoke(VALUE_ONE))
-        assertEquals("Value [VALUE_TWO]", SpaceDefaults.defaultItemDescriber<TestingSpace>().invoke(VALUE_TWO))
+        assertEquals("Value [VALUE_ONE]", SpaceDefaults.defaultItemDescriber<TestingEnum>().invoke(VALUE_ONE))
+        assertEquals("Value [VALUE_TWO]", SpaceDefaults.defaultItemDescriber<TestingEnum>().invoke(VALUE_TWO))
     }
 
     @Test
     fun testDefaultItem() {
-        assertEquals(setOf(VALUE_ONE, VALUE_TWO), SpaceDefaults.defaultItems(TestingSpace::class.java))
-    }
-
-    enum class TestingSpace {
-        VALUE_ONE,
-        VALUE_TWO;
-
-        override fun toString(): String = "Value [$name]"
+        assertEquals(setOf(VALUE_ONE, VALUE_TWO), SpaceDefaults.defaultItems(TestingEnum::class.java))
     }
 }
