fd563ee384c6a83c94c8eb583fc9a674cac19797
Paul Cooper <pdcooper80@gmail.com>
2024-08-13 21:05:34 +0100

    #3: Define spaces

--- core/src/main/kotlin/online/expotential/finite/core/spaces/StandardFiniteSpace.kt
+++ core/src/main/kotlin/online/expotential/finite/core/spaces/StandardFiniteSpace.kt
@@ -2,10 +2,11 @@ package online.expotential.finite.core.spaces
 
 data class StandardFiniteSpace<T> internal constructor(
     private val description: String,
-    private val itemDescriber: ItemDescriber<T>,
-    private val items: Set<T>
+    private val items: Set<T>,
+    private val itemDescriber: ItemDescriber<T>
 ) : FiniteSpace<T> {
     override fun description(): String = description
     override fun describeItem(item: T): String = itemDescriber.invoke(item)
     override fun items(): Set<T> = items
+    override fun toString(): String = "Space [$description] containing items $items"
 }
---
+++ core/src/test/kotlin/online/expotential/finite/core/spaces/StandardFiniteSpaceTest.kt
@@ -0,0 +1,33 @@
+package online.expotential.finite.core.spaces
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertSame
+import org.junit.jupiter.api.Test
+
+class StandardFiniteSpaceTest {
+
+    @Test
+    fun testDescription() {
+        val space = StandardFiniteSpace<Int>("Hello world", setOf()) { "" }
+        assertEquals("Hello world", space.description())
+    }
+
+    @Test
+    fun testDescribeItem() {
+        val space = StandardFiniteSpace<Int>("Hello world", setOf()) { "Number $it" }
+        assertEquals("Number 5", space.describeItem(5))
+    }
+
+    @Test
+    fun testItems() {
+        val items = setOf(3, 7)
+        val space = StandardFiniteSpace("Hello world", items) { "Number $it" }
+        assertSame(items, space.items())
+    }
+
+    @Test
+    fun testToString() {
+        val space = StandardFiniteSpace("Hello world", setOf(1, 5)) { "Number $it" }
+        assertEquals("Space [Hello world] containing items [1, 5]", space.toString())
+    }
+}
