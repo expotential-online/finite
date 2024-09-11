250d2a6e489251cea288850d6aa2e5acaf5bde4a
pdcooper80 <pdcooper80@gmail.com>
2024-09-11 18:24:00 +0100

    #11: Describe space unit tests more consistently (#12)

--- core/src/test/kotlin/online/expotential/finite/core/spaces/StandardFiniteSpaceTest.kt
+++ core/src/test/kotlin/online/expotential/finite/core/spaces/StandardFiniteSpaceTest.kt
@@ -2,23 +2,28 @@ package online.expotential.finite.core.spaces
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertSame
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
+@DisplayName("A standard finite space")
 class StandardFiniteSpaceTest {
 
     @Test
+    @DisplayName("should give the expected description")
     fun testDescription() {
         val space = StandardFiniteSpace(Int::class.java, "Hello world", setOf()) { "" }
         assertEquals("Hello world", space.description())
     }
 
     @Test
+    @DisplayName("should describe an item in the expected way")
     fun testDescribeItem() {
         val space = StandardFiniteSpace(Int::class.java, "Hello world", setOf()) { "Number $it" }
         assertEquals("Number 5", space.describeItem(5))
     }
 
     @Test
+    @DisplayName("should return all items contained in the space")
     fun testItems() {
         val items = setOf(3, 7)
         val space = StandardFiniteSpace(Int::class.java, "Hello world", items) { "Number $it" }
@@ -26,6 +31,7 @@ class StandardFiniteSpaceTest {
     }
 
     @Test
+    @DisplayName("should have a meaningful string representation")
     fun testToString() {
         val space = StandardFiniteSpace(Int::class.java, "Hello world", setOf(1, 5)) { "Number $it" }
         assertEquals("Space [Hello world] containing items [1, 5]", space.toString())
