6595799993fe00f50d52f9d10289a8a6583f1d79
pdcooper80 <pdcooper80@gmail.com>
2024-11-09 17:27:53 +0000

    #22: Address technical debt (#23)

--- scala/src/main/kotlin/online/expotential/finite/scala/spaces/builders/ScalaSpaceBuilders.kt
+++ scala/src/main/kotlin/online/expotential/finite/scala/spaces/builders/ScalaSpaceBuilders.kt
@@ -1,23 +1,20 @@
 package online.expotential.finite.scala.spaces.builders
 
-import mu.KLogger
-import mu.KotlinLogging.logger
 import online.expotential.finite.core.spaces.builders.FiniteSpaceBuilder
 import online.expotential.finite.core.spaces.builders.StandardFiniteSpaceBuilder
 import online.expotential.finite.scala.ScalaUtil.MODULE_FIELD_NAME
-import java.util.Objects
+import java.util.*
 
 object ScalaSpaceBuilders {
-
-    private val logger: KLogger = logger { }
 
     @JvmStatic
     fun <T> spaceForObjectOfObjects(caseClass: Class<T>): FiniteSpaceBuilder<T> {
         val items = caseClass.declaredClasses
+            .asSequence()
             .filter { caseClass == it.superclass }
             .map { c ->
                 try {
-                    caseClass.cast(c.getDeclaredField(MODULE_FIELD_NAME).get(c))
+                    caseClass.cast(c.getDeclaredField(MODULE_FIELD_NAME)[c])
                 } catch (e: NoSuchFieldException) {
                     null
                 }
